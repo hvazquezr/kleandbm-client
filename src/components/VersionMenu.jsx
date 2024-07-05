0c8ce6bf8e1b70d23c8c38262ef89d4ca4ecf229
Hector Vazquez <hector.vazquez@jaesis.com>
2024-07-05 02:29:43 -0400

    Ability to rename version names from history page. Removed unnecessary component dependencies from Project Info

--- src/components/VersionMenu.jsx
+++ src/components/VersionMenu.jsx
@@ -8,10 +8,45 @@ import DriveFileRenameOutlineIcon from '@mui/icons-material/DriveFileRenameOutli
 import FormatClearIcon from '@mui/icons-material/FormatClear';
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import { useSnackbar } from 'notistack';
+
+
 
 export default function VersionMenu({versionMenuOptions, onClose, onUpdateChangeName}){
-    const open = Boolean(versionMenuOptions);
-    console.log(versionMenuOptions);
+    const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+
+    const [openVersionName, setOpenVersionName] = React.useState(false);
+    const [versionName, setVersionName] = React.useState(null);
+    const [changeNameError, setChangeNameError] = React.useState("");
+
+    React.useEffect(() => {
+        setVersionName(versionMenuOptions.change.name || "");
+      }, [versionMenuOptions]);
+
+    const handleRename = useCallback(() => {
+        //setVersionName(versionMenuOptions.change.name || "");
+        setOpenVersionName(true);
+    }, [versionMenuOptions]);
+
+    const handleRenameSubmit = () => {
+        closeSnackbar();
+        let error = ""
+        if (!versionName) {
+            error = 'Version name is required.'
+            enqueueSnackbar(error, {variant: 'error'});  
+            setChangeNameError(error);
+            return;
+        }
+        setChangeNameError("");
+        onUpdateChangeName(versionMenuOptions.change.id, versionName);
+        onClose();
+    };
 
     const handleRemoveName = useCallback(() => {
         onUpdateChangeName(versionMenuOptions.change.id, null);
@@ -19,37 +54,66 @@ export default function VersionMenu({versionMenuOptions, onClose, onUpdateChange
     }, [versionMenuOptions]);
 
     return (
-        <Menu
-            open={open}
-            anchorEl={versionMenuOptions?.anchorEl}
-            onClose={onClose}
-        >   
-            <MenuItem >
-                <ListItemIcon>
-                    <SettingsBackupRestoreIcon fontSize="small" />
-                </ListItemIcon>
-                <ListItemText>Restore this version</ListItemText>
-            </MenuItem>
-            <MenuItem >
-                <ListItemIcon>
-                    <DriveFileRenameOutlineIcon fontSize="small" />
-                </ListItemIcon>
-                <ListItemText>Rename</ListItemText>
-            </MenuItem>
-            {versionMenuOptions.change.name &&
-            <MenuItem onClick={handleRemoveName} >
-                <ListItemIcon>
-                    <FormatClearIcon fontSize="small" />
-                </ListItemIcon>
-                <ListItemText>Remove name</ListItemText>
-            </MenuItem>
-            }
-            <MenuItem >
-                <ListItemIcon>
-                    <ContentCopyIcon fontSize="small" />
-                </ListItemIcon>
-                <ListItemText>Make a copy</ListItemText>
-            </MenuItem>
-        </Menu>
+        <React.Fragment>
+            <Menu
+                open={true}
+                anchorEl={versionMenuOptions?.anchorEl}
+                onClose={onClose}
+            >   
+                <MenuItem >
+                    <ListItemIcon>
+                        <SettingsBackupRestoreIcon fontSize="small" />
+                    </ListItemIcon>
+                    <ListItemText>Restore this version</ListItemText>
+                </MenuItem>
+                <MenuItem onClick={handleRename} >
+                    <ListItemIcon>
+                        <DriveFileRenameOutlineIcon fontSize="small" />
+                    </ListItemIcon>
+                    <ListItemText>Rename</ListItemText>
+                </MenuItem>
+                {versionMenuOptions.change.name &&
+                <MenuItem onClick={handleRemoveName} >
+                    <ListItemIcon>
+                        <FormatClearIcon fontSize="small" />
+                    </ListItemIcon>
+                    <ListItemText>Remove name</ListItemText>
+                </MenuItem>
+                }
+                <MenuItem >
+                    <ListItemIcon>
+                        <ContentCopyIcon fontSize="small" />
+                    </ListItemIcon>
+                    <ListItemText>Make a copy</ListItemText>
+                </MenuItem>
+            </Menu>
+            <Dialog
+                open={openVersionName}
+            >
+                <DialogContent>
+                <DialogContentText>
+                    Assign a name to this version to easily identify it.
+                </DialogContentText>
+                <TextField
+                    autoFocus
+                    required
+                    margin="dense"
+                    id="name"
+                    name="versionName2"
+                    label="Version Name"
+                    type="text"
+                    fullWidth
+                    variant="standard"
+                    value={versionName} // Set the value of the TextField
+                    onChange={(e) => setVersionName(e.target.value)} 
+                    error={changeNameError !== ""}
+                />
+                </DialogContent>
+                <DialogActions>
+                <Button onClick={() => {setOpenVersionName(false); onClose();}}>Cancel</Button>
+                <Button variant="contained" onClick={handleRenameSubmit}>Submit</Button>
+                </DialogActions>
+            </Dialog>
+        </React.Fragment>
     );
 }
